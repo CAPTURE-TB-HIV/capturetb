fe897b5324361358b54bbcccb9dd4467d11621c9
Alex Hill <alex.hill@gmail.com>
2026-06-08 18:41:38 +0100

    add tests, update vignettes and increment version

--- tests/testthat/test-unitcost.R
+++ tests/testthat/test-unitcost.R
@@ -29,7 +29,7 @@ test_that("can load total unit cost model", {
   expect_equal(mod$target(), "ID_unitcost_total")
   n_eff <- mod$n_eff()
   n_eff <- n_eff[!sapply(names(n_eff), grepl, pattern = "(^fc)")]
-#  expect_true(all(n_eff > 20000))
+  #  expect_true(all(n_eff > 20000))
 })
 
 test_that("can load fixed unitcost model", {
@@ -107,4 +107,117 @@ test_that("can get DIC", {
   expect_true(inherits(unitcost()$mcmc_DIC(), "numeric"))
   expect_true(inherits(unitcost_fixed()$mcmc_DIC(), "numeric"))
   expect_true(inherits(unitcost_ohd()$mcmc_DIC(), "numeric"))
+})
+
+test_that("can get extended unit cost model for financial costs", {
+  mod_fin <- unitcost_extended("FIN")
+  expect_true(all(mod_fin$training_data()$met_FINvECON == "FIN"))
+  expect_equal(mod_fin$covariates(), c(
+    "public",
+    "urban",
+    "healthcentre",
+    "primary",
+    "secondary",
+    "tertiary",
+    "logVisits",
+    "logVisitsPP_TB",
+    "log_ID_p_bldgspace"
+  ))
+  expect_true(!mod_fin$is_fitted())
+  expect_equal(mod_fin$target(), "ID_unitcost_total")
+})
+
+test_that("gets extended unit cost model for econ costs by default", {
+  mod <- unitcost_extended()
+  expect_equal(mod$covariates(), c(
+    "public",
+    "urban",
+    "healthcentre",
+    "primary",
+    "secondary",
+    "tertiary",
+    "logVisits",
+    "logVisitsPP_TB",
+    "log_ID_p_bldgspace"
+  ))
+
+  expect_true(all(mod$training_data()$met_FINvECON == "ECON"))
+  expect_true(!mod$is_fitted())
+  expect_equal(mod$target(), "ID_unitcost_total")
+})
+
+test_that("can get extended fixed unit cost model for financial costs", {
+  mod_fin <- unitcost_fixed_extended("FIN")
+  expect_equal(mod_fin$covariates(), c(
+    "public",
+    "urban",
+    "healthcentre",
+    "primary",
+    "secondary",
+    "tertiary",
+    "logVisits",
+    "logVisitsPP_TB",
+    "log_ID_p_bldgspace"
+  ))
+
+  expect_true(all(mod_fin$training_data()$met_FINvECON == "FIN"))
+  expect_true(!mod_fin$is_fitted())
+  expect_equal(mod_fin$target(), "ID_unitcost_fixed")
+})
+
+test_that("gets extended fixed unit cost model for econ costs by default", {
+  mod <- unitcost_fixed_extended()
+  expect_equal(mod$covariates(), c(
+    "public",
+    "urban",
+    "healthcentre",
+    "primary",
+    "secondary",
+    "tertiary",
+    "logVisits",
+    "logVisitsPP_TB",
+    "log_ID_p_bldgspace"
+  ))
+
+  expect_true(all(mod$training_data()$met_FINvECON == "ECON"))
+  expect_true(!mod$is_fitted())
+  expect_equal(mod$target(), "ID_unitcost_fixed")
+})
+
+test_that("can get extended ohd unit cost model for financial costs", {
+  mod_fin <- unitcost_ohd_extended("FIN")
+  expect_equal(mod_fin$covariates(), c(
+    "public",
+    "urban",
+    "healthcentre",
+    "primary",
+    "secondary",
+    "tertiary",
+    "logVisits",
+    "logVisitsPP_TB",
+    "log_ID_p_bldgspace"
+  ))
+
+  expect_true(all(mod_fin$training_data()$met_FINvECON == "FIN"))
+  expect_true(!mod_fin$is_fitted())
+  expect_equal(mod_fin$target(), "ID_unitcost_ohd")
+})
+
+test_that("gets extended ohd unit cost model for econ costs by default", {
+  mod <- unitcost_ohd_extended()
+  expect_equal(mod$covariates(), c(
+    "public",
+    "urban",
+    "healthcentre",
+    "primary",
+    "secondary",
+    "tertiary",
+    "logVisits",
+    "logVisitsPP_TB",
+    "log_ID_p_bldgspace"
+  ))
+
+  expect_true(all(mod$training_data()$met_FINvECON == "ECON"))
+  expect_true(!mod$is_fitted())
+  expect_equal(mod$target(), "ID_unitcost_ohd")
 })
